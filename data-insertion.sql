65d0bb21d45ab43f9228798f3498d5507f48ed52
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-09-17 11:37:12 +0600

    classroom data loaded

---
+++ data-insertion.sql
@@ -0,0 +1,46 @@
+/*********************************************************
+* Bangladesh Open University				
+* Dept. of Computer Science and Engineering		
+* S.C.: Dhaka University of Engineering & Technology	
+* Kazi Tajnur Islam	15-0-52-020-028 (Old Batch)	
+**********************************************************/
+
+/**********************************
+* 	   Data Insertion	  *
+***********************************/
+
+
+/*** DDL for inserting data into Classroom table ***/
+INSERT ALL
+    INTO classroom VALUES ('Watson', 7001, 80)
+    INTO classroom VALUES ('Watson', 7002, 80)
+    INTO classroom VALUES ('Watson', 7003, 80)
+    INTO classroom VALUES ('Watson', 7004, 60)
+    INTO classroom VALUES ('Watson', 7005, 60)
+    INTO classroom VALUES ('Watson', 5001, 80)
+    INTO classroom VALUES ('Watson', 5002, 80)
+    INTO classroom VALUES ('Watson', 5003, 80)
+    INTO classroom VALUES ('Watson', 5004, 60)
+    INTO classroom VALUES ('Watson', 5005, 60)
+    INTO classroom VALUES ('Watson', 3001, 80)
+    INTO classroom VALUES ('Watson', 3002, 80)
+    INTO classroom VALUES ('Watson', 3003, 80)
+    INTO classroom VALUES ('Watson', 3004, 60)
+    INTO classroom VALUES ('Watson', 3005, 60)
+    INTO classroom VALUES ('Packard', 4001, 80)
+    INTO classroom VALUES ('Packard', 4002, 80)
+    INTO classroom VALUES ('Packard', 4003, 60)
+    INTO classroom VALUES ('Packard', 4004, 60)
+    INTO classroom VALUES ('Packard', 3001, 80)
+    INTO classroom VALUES ('Packard', 3002, 80)
+    INTO classroom VALUES ('Packard', 3003, 60)
+    INTO classroom VALUES ('Packard', 3004, 60)
+    INTO classroom VALUES ('Taylor', 5001, 80)
+    INTO classroom VALUES ('Taylor', 5002, 80)
+    INTO classroom VALUES ('Taylor', 5003, 60)
+    INTO classroom VALUES ('Taylor', 5004, 60)
+    INTO classroom VALUES ('Taylor', 4001, 80)
+    INTO classroom VALUES ('Taylor', 4002, 80)
+    INTO classroom VALUES ('Taylor', 4003, 60)
+    INTO classroom VALUES ('Taylor', 4004, 60)       
+    SELECT * FROM dual;
